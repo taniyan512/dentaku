374376ccc0898bd629d4b7106edb706fab808cc2
EC2 Default User <ec2-user@ip-172-31-18-117.ec2.internal>
2022-03-21 03:50:11 +0000

    電卓

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function get_calc(btn) {
   let result = document.getElementById("display");
-  let operator = document.getElementById("operator")
+  let operator = document.getElementsByClassName('operator');
   let btn_value = btn.value;
+  let operator_value = operator.value;
       if(btn_value == "=") {
         result.value = eval(result.value);
       } else if (btn_value == "AC") {
@@ -12,8 +13,9 @@ function get_calc(btn) {
       } else if(result.value == "00") {
            result.value = btn_value
       } else {
-        if(result.value.slice(-1) == operator.value) {
-          result.value = result.value.slice(0,-1) + operator.value 
+        if(result.value.slice(-1) == operator_value) {
+          const f = result.value.slice(0,-1);
+          result.value = f + operator_value;
         } else {
           result.value += btn_value
         }  
