05abf6047c69f00671651dbf283d10bee9951b3c
EC2 Default User <ec2-user@ip-172-31-18-117.ec2.internal>
2022-03-22 20:42:26 +0000

    電卓課題

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 function get_calc(btn) {
   let result = document.getElementById("display");
-  let operator = document.getElementsByClassName('operator');
   let btn_value = btn.value;
-  let operator_value = operator.value;
+  const operator = ["+","-","*","/"];
       if(btn_value == "=") {
         result.value = eval(result.value);
       } else if (btn_value == "AC") {
@@ -13,12 +12,12 @@ function get_calc(btn) {
       } else if(result.value == "00") {
            result.value = btn_value
       } else {
-        if(result.value.slice(-1) == operator_value) {
+        if(operator.includes(result.value.slice(-1)) == true && operator.includes(btn_value) == true  ) {
           const f = result.value.slice(0,-1);
-          result.value = f + operator_value;
+          result.value = f + btn_value;
         } else {
-          result.value += btn_value
-        }  
-      } 
-      } 
+          result.value += btn_value;
+        }
+             }
+             }
 }
